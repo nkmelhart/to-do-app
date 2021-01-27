5c54a07fcce18b8691c3ba97a207f1c26fb6f0f5
Nolan Melhart <nkmelhart@gmail.com>
2021-01-27 04:31:12 -0600

    DRY version

--- app.js
+++ app.js
@@ -1,63 +1,76 @@
 const todos = [];
 
-const pendingClasses = 
-"my-4 bg-gray-800 w-full text-center text-indigo-500 rounded py-4 border-2 border-indigo-500 transition transform ease-in-out duration-900 hover:bg-indigo-500 hover:text-white hover:scale-110 cursor-pointer";
+const get = (elements) => 
+    elements.map((element) => document.getElementById(element));
 
-const completedClasses = 
-"my-4 bg-gray-800 w-full text-center text-yellow-500 rounded py-4 border-2 border-yellow-500 transition transform ease-in-out duration-900 hover:bg-yellow-500 hover:text-white hover:scale-110 cursor-pointer";
+const [pendingList, completedList, addForm, newTodo, deleteButton] = get
+    (["pendingList", "completedList", "addForm", "newTodo",
+        "deleteButton"]);
 
-const deleteButtonClass = "visible cursor-pointer inline bg-red-500 text-white mx-4 py-0 px-4 rounded hover:bg-red-700";
+const newList = [
+    {
+        element: pendingList,
+        status: "pending",
+    },
+    {
+        element: completedList,
+        status: "done",
+    }
+];
 
-const deleteButtonDisabledClass = "invisible"
+/*const modifyDeleteButton = get("deleteButton");
+const pendingList = get('pendingList');
+const completedList = get('completedList');
+const addForm = get("addForm");
+const newTodo = get("newTodo");
+const completedItemsHeader = get("completedItemsHeader");
+const pendingItemsHeader = get("pendingItemsHeader");
+const nothingHere = get("nothingHere");*/
+
+const cssClasses = {
+    pending: "my-4 bg-gray-800 w-full text-center text-indigo-500 rounded py-4 border-2 border-indigo-500 transition transform ease-in-out duration-900 hover:bg-indigo-500 hover:text-white hover:scale-110 cursor-pointer",
+    done: "my-4 bg-gray-800 w-full text-center text-yellow-500 rounded py-4 border-2 border-yellow-500 transition transform ease-in-out duration-900 hover:bg-yellow-500 hover:text-white hover:scale-110 cursor-pointer",
+    deleteButtonClass: "visible cursor-pointer inline bg-red-500 text-white mx-4 py-0 px-4 rounded hover:bg-red-700",
+    nothingHereVisibleClasses: "visible my-4 text-white animate-bounce",
+    nothingHereInvisibleClasses: "invisible my-4 text-white",
+    deleteButtonDisabledClass: "invisible",
+    completedItemsHeaderVisibleClasses: "visible text-2xl text-yellow-800 inline",
+    completedItemsHeaderInvisibleClasses: "invisible",
+    pendingItemsHeaderVisibleClasses: "visible text-2xl text-indigo-800 inline",
+    pendingItemsHeaderInvisibleClasses: "invisible",
+}
 
 function deleteButtonCheck() {
-    const modifyDeleteButton = document.getElementById("deleteButton");
     var clCount = todos.filter((todo) => todo.status === "done").length;
         if (clCount == 1){
-            modifyDeleteButton.className = deleteButtonClass;
-            modifyDeleteButton.textContent = "Delete";
+            deleteButton.className = cssClasses.deleteButtonClass;
+            deleteButton.textContent = "Delete";
         }
         else if (clCount >= 2){
-            modifyDeleteButton.className = deleteButtonClass;
-            modifyDeleteButton.textContent = "Delete All";
+            deleteButton.className = cssClasses.deleteButtonClass;
+            deleteButton.textContent = "Delete All";
         }
         else{
-            modifyDeleteButton.className = deleteButtonDisabledClass;
+            deleteButton.className = cssClasses.deleteButtonDisabledClass;
         }; 
-}
+};
 
+const updateTodos = () => {
+    newList.forEach((list) => {
+    const filteredTodos = todos.filter((todo) => todo.status === list.status);
 
-const pendingList = document.getElementById('pendingList');
-const completedList = document.getElementById('completedList');
-const deleteButton = document.getElementById('deleteButton');
+    list.element.innerHTML = ""; 
+    filteredTodos.forEach((todo) => {
+        const item = document.createElement("li");
+        item.className = cssClasses[list.status];
+        item.innerText = todo.text;
+        item.id = todo.id;
+        list.element.appendChild(item);
 
-const showTodos = () => {
-
-    const pendingTodos = todos.filter((todo => todo.status === 'pending'));
-
-    pendingList.innerHTML = ''; 
-    pendingTodos.forEach((todo)=> {
-        const pendingItem = document.createElement('li');
-        pendingItem.className = pendingClasses;
-        pendingItem.id = todo.id;
-        pendingItem.innerText = todo.text;
-        pendingList.appendChild(pendingItem);
     });
+});
+};
 
-    completedList.innerHTML = '';
-    const completedTodos = todos.filter((todo) => todo.status === "done");
-    completedTodos.forEach((todo) => {
-        const completedItem = document.createElement("li");
-        
-        completedItem.className = completedClasses;
-        completedItem.id = todo.id;
-        completedItem.innerText = todo.text;
-        completedList.appendChild(completedItem);
-})
-}
-
-const addForm = document.getElementById("addForm");
-const newTodo = document.getElementById("newTodo");
 addForm.addEventListener('submit', (event) => {
     event.preventDefault();
     if(newTodo.value === ""){
@@ -70,7 +83,11 @@ addForm.addEventListener('submit', (event) => {
         status: "pending",
     });
     newTodo.value = '';
-    showTodos();
+    nothingHere.className = cssClasses.nothingHereInvisibleClasses;
+    completedItemsHeader.className = cssClasses.completedItemsHeaderVisibleClasses;
+    pendingItemsHeader.className = cssClasses.pendingItemsHeaderVisibleClasses;
+
+    updateTodos();
 });
 
 pendingList.addEventListener('click', (event) => {
@@ -79,7 +96,7 @@ pendingList.addEventListener('click', (event) => {
     }
     todos.find((todo) => todo.id === event.target.id).status = "done";
         deleteButtonCheck();
-    showTodos();
+    updateTodos();
 });
 
 deleteButton.addEventListener('click', () => {
@@ -88,12 +105,22 @@ deleteButton.addEventListener('click', () => {
             todos.splice(i - 1, 1);
         }
     }
-    deleteButton.className = deleteButtonDisabledClass;
-    showTodos();
+    deleteButton.className = cssClasses.deleteButtonDisabledClass;
+    if (todos.length === 0){
+        console.log(todos.length);
+        completedItemsHeader.className = cssClasses.completedItemsHeaderInvisibleClasses;
+        pendingItemsHeader.className = cssClasses.pendingItemsHeaderInvisibleClasses;
+        nothingHere.className = cssClasses.nothingHereVisibleClasses;
+    }
+    else{
+        completedItemsHeader.className = cssClasses.completedItemsHeaderVisibleClasses;
+        pendingItemsHeader.classname = cssClasses.pendingItemsHeaderVisibleClasses;
+    };
+    updateTodos();
 });
 
 completedList.addEventListener('click', (event) => {
     todos.find((todo) => todo.id === event.target.id).status = "pending";
     deleteButtonCheck();
-    showTodos();
+    updateTodos();
 });
